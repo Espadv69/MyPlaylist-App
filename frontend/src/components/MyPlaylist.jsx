0e72a4f725449e1efe344c4991c36119cf1bfe24
Espadv69 <aitorjob69@gmail.com>
2025-03-23 16:29:57 +0100

    Add API_URL constant and update fetchPlaylist function in MyPlaylist component

--- frontend/src/components/MyPlaylist.jsx
+++ frontend/src/components/MyPlaylist.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import './MyPlaylist.css'
 
+const API_URL = 'http://localhost:5000/api/playlist'
+
 const MyPlaylist = () => {
   const [playlist, setPlaylist] = useState([])
   const [title, setTitle] = useState('')
@@ -10,7 +12,11 @@ const MyPlaylist = () => {
   const [duration, setDuration] = useState('')
   const [youtubeId, setYoutubeId] = useState('')
 
-  const fetchPlaylist = async () => {}
+  const fetchPlaylist = async () => {
+    try {
+      const response = await fetch(/* Add */)
+    } catch (err) {}
+  }
 
   useEffect(() => {
     fetchPlaylist()
