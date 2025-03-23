78cb9c19644b0e5b70adc305e1d3773344f53139
Espadv69 <aitorjob69@gmail.com>
2025-03-23 16:26:29 +0100

    Add state management for MyPlaylist component

--- frontend/src/components/MyPlaylist.jsx
+++ frontend/src/components/MyPlaylist.jsx
@@ -1,6 +1,14 @@
 import { useState } from 'react'
 import './MyPlaylist.css'
 
-const MyPlaylist = () => {}
+const MyPlaylist = () => {
+  const [playlist, setPlaylist] = useState([])
+  const [title, setTitle] = useState('')
+  const [artist, setArtist] = useState('')
+  const [genre, setGenre] = useState('')
+  const [album, setAlbum] = useState('')
+  const [duration, setDuration] = useState('')
+  const [youtubeId, setYoutubeId] = useState('')
+}
 
 export default MyPlaylist
