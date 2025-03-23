e66ae6ce3b8f9e0a1d95164cb842c0efba97ed94
Espadv69 <aitorjob69@gmail.com>
2025-03-23 11:54:17 +0100

    Add Mongoose integration and dotenv configuration to server setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 
+require('dotenv').config()
+
 const app = express()
+
+// Middleware
 app.use(cors())
 app.use(express.json())
