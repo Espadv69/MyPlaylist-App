476882bbec9c18c10a73dc32b4e67d9e6d6e42d1
Espadv69 <aitorjob69@gmail.com>
2025-03-24 11:58:12 +0100

    Add myPlaylistRouteID constant for dynamic playlist route handling

--- backend/server/routes/const.js
+++ backend/server/routes/const.js
@@ -1,5 +1,7 @@
 const home = '/'
 const myPlaylist = '/api/playlists'
+const myPlaylistRouteID = '/api/playlists/:id'
 
 exports.home = home
 exports.myPlaylist = myPlaylist
+exports.myPlaylistRouteID = myPlaylistRouteID
